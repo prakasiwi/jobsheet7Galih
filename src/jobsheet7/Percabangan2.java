43e374c917c4ac4f560506959f90dbe692c7e8d2
galih <galih@DESKTOP-DMHAKJQ>
2018-08-03 12:52:25 +0700

    Tambah Tampil Resep

--- src/jobsheet7/Percabangan2.java
+++ src/jobsheet7/Percabangan2.java
@@ -46,6 +46,36 @@ public class Percabangan2 {
             System.out.println("Mohon Maaf, Pilihan Tidak Ditemukan, " 
                     + "tidak dapt memberikan saran resep");
         
+        int bahan2 = scanner.nextInt();
+        
+        if (bahan2 >= 1 && bahan2 <= 3)
+        {
+            if(bahan1==1)
+                switch(bahan2)
+                {
+                    case 1: System.out.println(
+                            "Anda dapat membuat milk shake banana"); break;
+                    case 2: System.out.println(
+                            "Anda dapat membuat pisang goreng"); break;
+                    case 3: System.out.println(
+                            "Anda dapat membuat pisang rebus");        
+                }
+            else
+                switch(bahan2)
+                {
+                    case 1: System.out.println(
+                            "Anda dapat membuat telur mata sapi"); break;
+                    case 2: System.out.println(
+                            "Anda dapat membuat sandwich telur"); break;
+                    case 3: System.out.println(
+                            "Anda dapat membuat telur rebus");  
+                    
+                }
+            
+        }else
+            System.out.println("Mohon Maaf, Pilihan Tidak Ditemukan, " 
+                    + "tidak dapt memberikan saran resep");
+                    
         }    
     }
     
