1342736ff45439c43899b99983494f354c57c65c
galih <galih@DESKTOP-DMHAKJQ>
2018-08-03 11:36:00 +0700

    Cooba SWITCH..CASE.

--- src/jobsheet7/LaithanCobaPercabangan1.java
+++ src/jobsheet7/LaithanCobaPercabangan1.java
@@ -65,7 +65,14 @@ public class LaithanCobaPercabangan1 {
         if (a < 4) 
             System.out.println("nilai a kurang dari 4");
         }
-         
-        
+        System.out.println("\nSWITCH..CASE"); 
+        switch(a) {
+            case 1 :  System.out.println("nilai a  => 1"); break;
+            case 2 :  System.out.println("nilai a => 2"); break;
+            case 3 :  
+            case 4 :  System.out.println("nilai a => 3 atau 4"); break;
+            case 5 :  System.out.println("nilai a => 5"); break;
+            default:  System.out.println("nilai a bukan antara 1-5"); 
+        }
     }
 }
