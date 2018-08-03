640996a28a526ccf87f0d650ca3441460fb9d07c
galih <galih@DESKTOP-DMHAKJQ>
2018-08-03 12:29:30 +0700

    Initial Commit

---
+++ src/jobsheet7/Percabangan2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+/**
+ *
+ * @author galih
+ */
+public class Percabangan2 {
+    public static void main(String[] args) {
+        String identitas = "Mochammad Galih Prakasiwi / X RPL 5 / 22";
+        System.out.println("identitas : "+ identitas);
+        
+        System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+    }
+    
+}
