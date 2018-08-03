6dd2138a8d5dacfcec2b6147500294b5096e1714
galih <galih@DESKTOP-DMHAKJQ>
2018-08-03 10:51:24 +0700

    coba IF

--- src/jobsheet7/LaithanCobaPercabangan1.java
+++ src/jobsheet7/LaithanCobaPercabangan1.java
@@ -30,5 +30,12 @@ public class LaithanCobaPercabangan1 {
         System.out.println("\nIF.. 3");
         if(a > 5)
             System.out.println("nilai a lebih dari 5");
+        
+        System.out.println("\nIF..ELSE..");
+        if (a<5) {
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a kurang dari 5");
+        } else System.out.println("nilai a lebih dari atau sama dengan 5");     
+        
     }
 }
